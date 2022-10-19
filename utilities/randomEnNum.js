74852dfa53d54ffeb17a849f979ab9f4cab87a7d
linway035 <wayneck0138@gmail.com>
2022-10-20 02:16:50 +0800

    feat: add utilities randomEnNum

---
+++ utilities/randomEnNum.js
@@ -0,0 +1,17 @@
+function randomEnNum(length_required) {
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseLetters = lowerCaseLetters.toUpperCase();
+  const numbers = "1234567890";
+  const collection = lowerCaseLetters + upperCaseLetters + numbers;
+
+  let randomText = "";
+  for (let i = 0; i < length_required; i++) {
+    const index = Math.floor(Math.random() * collection.length);
+    randomText += collection[index];
+  }
+  return randomText;
+}
+
+module.exports = randomEnNum;
+
+//shortid安全性不夠，nanoid不知怎用，base62普普
