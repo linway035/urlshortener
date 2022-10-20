790f7b3b0906022a42352b92685116636a5cba4d
linway035 <wayneck0138@gmail.com>
2022-10-20 10:47:05 +0800

    feat: add Schema

--- models/shortURL.js
+++ models/shortURL.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const shortUrlSchema = new Schema({
+  originalURL: { type: String, required: true },
+  shortURL: { type: String, required: true },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("shortURL", shortUrlSchema);
